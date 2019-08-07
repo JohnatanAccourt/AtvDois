846dd1d48101522c17bceb5165b71eb369d370c2
johnatan.asantos <johnatan1999accourt@gmail.com>
2019-08-07 17:17:05 -0300

    sbhvvuyv

---
+++ app/src/main/java/com/example/atvdois/Activity_Pagdois.java
@@ -0,0 +1,14 @@
+package com.example.atvdois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Pagdois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagdois_layout);
+    }
+}
---
+++ app/src/main/java/com/example/atvdois/Activity_Pagtres.java
@@ -0,0 +1,14 @@
+package com.example.atvdois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Pagtres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagtres_layout);
+    }
+}
